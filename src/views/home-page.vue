b9d6d152b40503ad4b544a013baac04ca9c4b276
omar amer <omar.amer.asfor.96@gmail.com>
2021-12-01 16:17:36 +0200

    added explore

--- src/views/home-page.vue
+++ src/views/home-page.vue
@@ -1,6 +1,74 @@
 <template>
   <div class="home">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+
+
+
+ <section class="gallery-header">
+      <h2 class="head">Explore nearby</h2>
+      <a href="#/explore">Show more</a>
+    </section>
+      <section class="gallery">
+        <a href="#/stay/4567">
+          <div>
+            <img src="../assets/imgs/logo.png" />
+            <h4 class="flex">house</h4>
+          </div>
+        </a>
+        <a href="#/stay/4567">
+          <div>
+            <img src="../assets/imgs/logo.png" />
+            <h4>villa</h4>
+          </div>
+        </a>
+        <a href="#/stay/4567">
+          <div>
+            <img src="../assets/imgs/logo.png" />
+            <h4>vedio</h4>
+          </div>
+        </a>
+        <a href="#/stay/4567">
+          <div>
+            <img src="../assets/imgs/logo.png" />
+            <h4>castle</h4>
+          </div>
+        </a>
+      </section>
+
+
+    <section class="gallery-header">
+      <h2 class="head">Top Rated</h2>
+      <a href="#/explore">Show more</a>
+    </section>
+      <section class="gallery">
+        <a href="#/stay/4567">
+          <div>
+            <img src="../assets/imgs/logo.png" />
+            <h4 class="flex">house</h4>
+          </div>
+        </a>
+        <a href="#/stay/4567">
+          <div>
+            <img src="../assets/imgs/logo.png" />
+            <h4>villa</h4>
+          </div>
+        </a>
+        <a href="#/stay/4567">
+          <div>
+            <img src="../assets/imgs/logo.png" />
+            <h4>vedio</h4>
+          </div>
+        </a>
+        <a href="#/stay/4567">
+          <div>
+            <img src="../assets/imgs/logo.png" />
+            <h4>castle</h4>
+          </div>
+        </a>
+      </section>
+
+
   </div>
 </template>
 
