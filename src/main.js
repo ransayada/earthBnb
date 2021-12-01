d6ac34089865798cb621271d86aac81ee14ba6b4
eriaidian <eriadian@gmail.com>
2021-12-01 16:11:36 +0200

    added footer

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import '@/assets/main.scss'
+import '@fortawesome/fontawesome-free/css/all.css';
+import '@fortawesome/fontawesome-free/js/all.js';
+
 
 Vue.config.productionTip = false
 
