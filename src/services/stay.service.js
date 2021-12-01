175ab92ce83f396ea53683239e7c913b341950d6
eriaidian <eriadian@gmail.com>
2021-12-01 17:26:24 +0200

    added stay store

--- src/services/stay.service.js
+++ src/services/stay.service.js
@@ -1,1 +1,119 @@
-import Axios from 'axios';
+import { storageService } from './async-storage.service.js'
+import { utilService } from './util.service.js'
+// const axios = require('axios');
+const KEY = 'stayDB';
+// const STAY_URL = '/api/stay/'
+import Axios from 'axios'
+// var axios = Axios.create({
+//     withCredentials: true
+// })
+
+// const TOY_URL = '//localhost:3030/api/stay/'
+
+// const TOY_URL = (process.env.NODE_ENV !== 'development')
+//     ? '/api/stay'
+//     : '//localhost:3030/api/stay';
+
+export const stayService = {
+    query,
+    getById,
+    remove,
+    save,
+    getEmptyStay
+}
+
+// const labels = ["On wheels", "Box game", "Art", "Baby", "Doll", "Puzzle", "Outdoor"]
+
+
+_createStays()
+
+// TODO: support paging and filtering and sorting
+async function query() {
+    return storageService.query(KEY)
+    //  return axios.get(TOY_URL).then(res => res.data)
+    // const res = await axios.get(TOY_URL)
+    // return res.data
+}
+
+async function getById(id) {
+    return storageService.get(KEY, id)
+    // return axios.get(TOY_URL + id).then(res => res.data)
+    // const res = await axios.get(TOY_URL + id)
+    // return res.data
+}
+
+async function remove(id) {
+    return storageService.remove(KEY, id)
+    // return axios.delete(TOY_URL + id).then(res => res.data)
+    // const res = await axios.delete(TOY_URL + id)
+    // return res.data
+}
+
+async function save(stay) {
+    const savedStay = (stay._id) ? storageService.put(KEY, stay) : storageService.post(KEY, stay)
+    return savedStay;
+    // if (stay._id) {
+    //     console.log('i am here');
+    //     // return axios.put(TOY_URL, stay).then(res => res.data)
+    //     // const res = await axios.put(TOY_URL + stay._id, stay)
+
+    //     return res.data
+    // } else {
+    //     // return axios.post(TOY_URL, stay).then(res => res.data)
+    //     // const res = await axios.post(TOY_URL, stay)
+    //     // console.log(res.data);
+    //     return res.data
+    // }
+}
+
+function getEmptyStay(name = '', price = 100,) {
+    return {
+        _id: '',
+        name,
+        price,
+        imgUrls: [],
+        summary: '',
+        createdAt: null,
+        reviews: [],
+        propertyType: '',
+        capacity: 0,
+        amenities: [],
+        host: {
+            _id: "",
+            fullName: "",
+            imgUrl: "",
+        },
+        loc: {
+            country: "",
+            countryCode: "",
+            address: "",
+            lat: 0,
+            lng: 0
+        },
+
+    }
+}
+
+
+// Create Test Data:
+function _createStays() {
+    var stays = JSON.parse(localStorage.getItem(KEY))
+    if (!stays || !stays.length) {
+        stays = [
+                _createStay('Buzz'),
+                _createStay('woodie'),
+                _createStay('tmnt'),
+            ]
+        localStorage.setItem(KEY, JSON.stringify(stays))
+    }
+    return stays;
+}
+
+function _createStay(name) {
+    const stay = getEmptyStay(name, utilService.getRandomInt(80, 300))
+    stay._id = utilService.makeId()
+    stay.reviews.push({txt: 'good stay'})
+    stay.createdAt = Date.now().toLocaleString()
+    stay.imgUrls = ['../assets/logo.png','../assets/logo.png','../assets/logo.png','../assets/logo.png','../assets/logo.png']
+    return stay
+}
--- src/store/stay.store.js
+++ src/store/stay.store.js
@@ -1,8 +1,100 @@
 // import { stayService } from '../services/stay.service.js';
 
 export const stayStore = {
-    state: {},
-    mutations: {},
-    actions: {},
-    getters: {}
+    state: {
+        stays: [],
+        filterBy: {
+            name: '',
+            labels: [],
+            amenities: [],
+            typeOfPlace: '',
+            price: {
+                fromPrice: -Infinity,
+                toPrice: Infinity
+            }
+        },
+        currStay: null,
+
+    },
+    mutations: {
+        setStays(state, { stays }) {
+            state.stays = stays
+        },
+        addStay(state, { saveStay }) {
+            state.stays.push(saveStay)
+        },
+        updateStay(state, { saveStay }) {
+            const idx = state.stays.findIndex(currStay => currStay._id === saveStay._id)
+            state.stays.splice(idx, 1, saveStay)
+        },
+        removeStay(state, { stayId }) {
+            const idx = state.stays.findIndex(currStay => currStay._id === stayId)
+            state.stays.splice(idx, 1)
+        },
+        setFilterBy(state, { filterBy }) {
+            state.filterBy = filterBy
+        },
+        setCurrStay(state, { stay }) {
+            console.log('currStay', state.currStay)
+            state.currStay = stay
+            console.log('currStay', state.currStay)
+        },
+
+
+    },
+    actions: {
+        loadStays({ commit, state }) {
+            return stayService.query(state.filterBy)
+                .then((stays) => {
+                    commit({ type: 'setStays', stays })
+                })
+        },
+        addStay({ commit }, { stay }) {
+            return stayService.save(stay)
+                .then((saveStay) => {
+                    commit({ type: 'addStay', saveStay })
+                })
+        },
+        updateStay({ commit }, { stay }) {
+            return stayService.save(stay)
+                .then((saveStay) => {
+                    commit({ type: 'updateStay', saveStay })
+                })
+                .catch(() => {
+                    console.log('didnt update product');
+                })
+        },
+        removeStay({ commit }, { stayId }) {
+            return stayService.remove(stayId)
+                .then(() => {
+                    commit({ type: 'removeStay', stayId })
+
+                })
+        },
+        setCurrStay({ commit }, { stayId }) {
+            return stayService.getById(stayId).then((stay) => {
+                console.log(stay)
+                commit({ type: 'setCurrStay', stay })
+            })
+        },
+
+
+    },
+    getters: {
+        staysToShow(state) {
+            return state.stays
+        },
+        totalStays(state) {
+            return state.stays.length
+        },
+        // totalInStock(state) { //Type of Place???????????
+        //     return state.stays.map((stay => stay.inStock))
+        // },
+        // totalOutStock(state) {
+        //     return state.stays.map((stay => !stay.inStock))
+        // },
+        getCurrStay(state) {
+            return state.currStay
+        }
+    }
 }
