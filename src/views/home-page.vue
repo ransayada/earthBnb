1f2ee262bf8c4fae55891c8bb4bf5281c9d30d67
omar amer <omar.amer.asfor.96@gmail.com>
2021-12-01 16:30:48 +0200

    basic homepage fixed

--- src/views/home-page.vue
+++ src/views/home-page.vue
@@ -1,90 +1,79 @@
 <template>
   <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="hero-container">
+      <img src="http://" alt="" />
+      <a href="https://"><p>Become a host</p></a>
+    </div>
 
-
-
-
- <section class="gallery-header">
+    <section class="gallery-header">
       <h2 class="head">Explore nearby</h2>
       <a href="#/explore">Show more</a>
     </section>
-      <section class="gallery">
-        <a href="#/stay/4567">
-          <div>
-            <img src="../assets/imgs/logo.png" />
-            <h4 class="flex">house</h4>
-          </div>
-        </a>
-        <a href="#/stay/4567">
-          <div>
-            <img src="../assets/imgs/logo.png" />
-            <h4>villa</h4>
-          </div>
-        </a>
-        <a href="#/stay/4567">
-          <div>
-            <img src="../assets/imgs/logo.png" />
-            <h4>vedio</h4>
-          </div>
-        </a>
-        <a href="#/stay/4567">
-          <div>
-            <img src="../assets/imgs/logo.png" />
-            <h4>castle</h4>
-          </div>
-        </a>
-      </section>
-
+    <section class="gallery">
+      <a href="#/stay/4567">
+        <div>
+          <img src="../assets/imgs/logo.png" />
+          <h4 class="flex">house</h4>
+        </div>
+      </a>
+      <a href="#/stay/4567">
+        <div>
+          <img src="../assets/imgs/logo.png" />
+          <h4>villa</h4>
+        </div>
+      </a>
+      <a href="#/stay/4567">
+        <div>
+          <img src="../assets/imgs/logo.png" />
+          <h4>vedio</h4>
+        </div>
+      </a>
+      <a href="#/stay/4567">
+        <div>
+          <img src="../assets/imgs/logo.png" />
+          <h4>castle</h4>
+        </div>
+      </a>
+    </section>
 
     <section class="gallery-header">
       <h2 class="head">Top Rated</h2>
       <a href="#/explore">Show more</a>
     </section>
-      <section class="gallery">
-        <a href="#/stay/4567">
-          <div>
-            <img src="../assets/imgs/logo.png" />
-            <h4 class="flex">house</h4>
-          </div>
-        </a>
-        <a href="#/stay/4567">
-          <div>
-            <img src="../assets/imgs/logo.png" />
-            <h4>villa</h4>
-          </div>
-        </a>
-        <a href="#/stay/4567">
-          <div>
-            <img src="../assets/imgs/logo.png" />
-            <h4>vedio</h4>
-          </div>
-        </a>
-        <a href="#/stay/4567">
-          <div>
-            <img src="../assets/imgs/logo.png" />
-            <h4>castle</h4>
-          </div>
-        </a>
-      </section>
-
-
-  <div class="hero-container">
-   <img src="http://" alt="">
-  <a href="https://"><p>Become a host</p></a>
-  </div>
-    <HelloWorld msg="temporary app body :)"/>
+    <section class="gallery">
+      <a href="#/stay/4567">
+        <div>
+          <img src="../assets/imgs/logo.png" />
+          <h4 class="flex">house</h4>
+        </div>
+      </a>
+      <a href="#/stay/4567">
+        <div>
+          <img src="../assets/imgs/logo.png" />
+          <h4>villa</h4>
+        </div>
+      </a>
+      <a href="#/stay/4567">
+        <div>
+          <img src="../assets/imgs/logo.png" />
+          <h4>vedio</h4>
+        </div>
+      </a>
+      <a href="#/stay/4567">
+        <div>
+          <img src="../assets/imgs/logo.png" />
+          <h4>castle</h4>
+        </div>
+      </a>
+    </section>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/cmps/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  components: {},
+};
 </script>
