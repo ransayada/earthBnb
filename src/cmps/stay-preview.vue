9ebad747b4a209372d95244edd767418bba95437
omar amer <omar.amer.asfor.96@gmail.com>
2021-12-06 09:44:21 +0200

    fixed carousel background

--- src/cmps/stay-preview.vue
+++ src/cmps/stay-preview.vue
@@ -117,11 +117,9 @@ export default {
   margin: 0;
 }
 
-.el-carousel__item:nth-child(2n) {
-  background-color: #99a9bf;
+.el-carousel__item {
+  background-color: #ffffff;
 }
 
-.el-carousel__item:nth-child(2n + 1) {
-  background-color: #d3dce6;
-}
+
 </style>
