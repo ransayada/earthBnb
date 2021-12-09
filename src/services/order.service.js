f5a9fd2615a3300415cfe777f909442e46b753b8
omar amer <omar.amer.asfor.96@gmail.com>
2021-12-09 10:46:56 +0200

    added connection to orders

--- src/services/order.service.js
+++ src/services/order.service.js
@@ -1,5 +1,6 @@
-import { storageService } from './async-storage.service.js'
+// import { storageService } from './async-storage.service.js'
 import { utilService } from './util.service.js'
+import { httpService } from './http.service'
 // const axios = require('axios');
 const KEY = 'orderDB';
 // const STAY_URL = '/api/order/'
@@ -25,33 +26,43 @@ export const orderService = {
 // const labels = ["On wheels", "Box game", "Art", "Baby", "Doll", "Puzzle", "Outdoor"]
 
 
-_createOrders()
+// _createOrders()
 
 // TODO: support paging and filtering and sorting
 async function query() {
-    return storageService.query(KEY)
+    // return storageService.query(KEY)
+
+    return httpService.get(KEY)
         //  return axios.get(TOY_URL).then(res => res.data)
         // const res = await axios.get(TOY_URL)
         // return res.data
 }
 
 async function getById(id) {
-    return storageService.get(KEY, id)
+    // return storageService.get(KEY, id)
+
+    return httpService.get(`order/${id}`)   
         // return axios.get(TOY_URL + id).then(res => res.data)
         // const res = await axios.get(TOY_URL + id)
         // return res.data
 }
 
 async function remove(id) {
-    return storageService.remove(KEY, id)
+    // return storageService.remove(KEY, id)
+
+    return httpService.delete(`order/${id}`)
         // return axios.delete(TOY_URL + id).then(res => res.data)
         // const res = await axios.delete(TOY_URL + id)
         // return res.data
 }
 
 async function save(order) {
-    const savedOrder = (order._id) ? storageService.put(KEY, order) : storageService.post(KEY, order)
-    return savedOrder;
+    // const savedOrder = (order._id) ? storageService.put(KEY, order) : storageService.post(KEY, order)
+    // return savedOrder;
+
+    const addedOrder = await httpService.post(`order`, order)
+
+    return addedOrder;
     // if (order._id) {
     //     console.log('i am here');
     //     // return axios.put(TOY_URL, order).then(res => res.data)
@@ -97,23 +108,23 @@ function getEmptyOrder() {
 
 
 // Create Test Data:
-function _createOrders() {
-    var orders = JSON.parse(localStorage.getItem(KEY))
-    if (!orders || !orders.length) {
-        orders = [
-            _createOrder(),
-            _createOrder(),
-            _createOrder(),
-        ]
-        localStorage.setItem(KEY, JSON.stringify(orders))
-    }
-    return orders;
-}
+// function _createOrders() {
+//     var orders = JSON.parse(localStorage.getItem(KEY))
+//     if (!orders || !orders.length) {
+//         orders = [
+//             _createOrder(),
+//             _createOrder(),
+//             _createOrder(),
+//         ]
+//         localStorage.setItem(KEY, JSON.stringify(orders))
+//     }
+//     return orders;
+// }
 
-function _createOrder() {
-    const order = getEmptyOrder();
-    order._id = utilService.makeId()
-    order.createdAt = Date.now();
-    order.imgUrls = ['https://res.cloudinary.com/sprint4rad/image/upload/v1638611196/Demo1/1/1_a55tgj.jpg', 'https://res.cloudinary.com/sprint4rad/image/upload/v1638611196/Demo1/1/7_agmyfv.jpg', 'https://res.cloudinary.com/sprint4rad/image/upload/v1638611196/Demo1/1/3_nvickq.jpg', 'https://res.cloudinary.com/sprint4rad/image/upload/v1638611196/Demo1/1/6_ybwvcs.jpg', 'https://res.cloudinary.com/sprint4rad/image/upload/v1638611196/Demo1/1/4_pwjrtl.jpg', 'https://res.cloudinary.com/sprint4rad/image/upload/v1638611195/Demo1/1/2_vv2wqn.jpg', 'https://res.cloudinary.com/sprint4rad/image/upload/v1638611195/Demo1/1/8_kd7vqz.jpg', 'https://res.cloudinary.com/sprint4rad/image/upload/v1638611195/Demo1/1/5_gk0j3m.jpg']
-    return order
-}
+// function _createOrder() {
+//     const order = getEmptyOrder();
+//     order._id = utilService.makeId()
+//     order.createdAt = Date.now();
+//     order.imgUrls = ['https://res.cloudinary.com/sprint4rad/image/upload/v1638611196/Demo1/1/1_a55tgj.jpg', 'https://res.cloudinary.com/sprint4rad/image/upload/v1638611196/Demo1/1/7_agmyfv.jpg', 'https://res.cloudinary.com/sprint4rad/image/upload/v1638611196/Demo1/1/3_nvickq.jpg', 'https://res.cloudinary.com/sprint4rad/image/upload/v1638611196/Demo1/1/6_ybwvcs.jpg', 'https://res.cloudinary.com/sprint4rad/image/upload/v1638611196/Demo1/1/4_pwjrtl.jpg', 'https://res.cloudinary.com/sprint4rad/image/upload/v1638611195/Demo1/1/2_vv2wqn.jpg', 'https://res.cloudinary.com/sprint4rad/image/upload/v1638611195/Demo1/1/8_kd7vqz.jpg', 'https://res.cloudinary.com/sprint4rad/image/upload/v1638611195/Demo1/1/5_gk0j3m.jpg']
+//     return order
+// }
