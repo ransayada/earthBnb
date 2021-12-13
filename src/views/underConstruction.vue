6037156d8b9775431de472841b776a4065961ea9
ransayada <ran.sayada@gmail.com>
2021-12-13 11:32:16 +0200

    demo

--- src/views/underConstruction.vue
+++ src/views/underConstruction.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about">
-    <h1>This function is on construction</h1>
+    <h1>This page is under construction</h1>
 
     <img src="https://res.cloudinary.com/sprint4rad/image/upload/v1639298340/158667_zaliyn.gif">
   </div>
